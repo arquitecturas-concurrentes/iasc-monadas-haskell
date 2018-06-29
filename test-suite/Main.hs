440083a157dc5b3e565e06225af8a7e793d45db3
juanFdS <juan9794@gmail.com>
2018-06-29 15:46:58 -0300

    Initial commit

---
+++ test-suite/Main.hs
@@ -0,0 +1,18 @@
+-- Tasty makes it easy to test your code. It is a test framework that can
+-- combine many different types of tests into one suite. See its website for
+-- help: <http://documentup.com/feuerbach/tasty>.
+import qualified Test.Tasty
+-- Hspec is one of the providers for Tasty. It provides a nice syntax for
+-- writing tests. Its website has more info: <https://hspec.github.io>.
+import Test.Tasty.Hspec
+import Data.Monoid
+
+main :: IO ()
+main = do
+    test <- testSpec "charla-monadas" spec
+    Test.Tasty.defaultMain test
+
+spec :: Spec
+spec = describe "tuvieja" $
+    it "is trivially true" $
+        (True `shouldBe` True) <> (True `shouldBe` False)
