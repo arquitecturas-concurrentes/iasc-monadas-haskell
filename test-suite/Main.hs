0ec71d07f28d54f3ccb93bb2d4559480990b89fc
Ernesto Bossi <bossi.ernestog@gmail.com>
2020-06-08 23:08:57 -0300

    Funciones Validadoras... se empieza a complicar

--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -44,4 +44,33 @@ spec = parallel $ do
 
         it "Deberia crearme el personaje cuando el nombre tiene la longitud correcta" $ do
             let nombrePersonaje = validarNombre "NicoKnight"
-            (construirPersonajeValidado nombrePersonaje 250) == Exito (Personaje 250 100 "NicoKnight")   
+            (construirPersonajeValidado nombrePersonaje 250) == Exito (Personaje 250 100 "NicoKnight")
+
+    describe "F(x)s adicionales" $ do
+        it "deberia devolverme el dinero seteado al personaje" $ do
+            let nombrePersonaje = "Arthas Menethil"
+            let personaje = Personaje 0 100 nombrePersonaje
+            obtenerDineroPersonaje personaje `shouldBe` 0
+
+        it "deberia devolverme las iniciales correctas del personaje" $ do
+            let nombrePersonaje = "Arthas Menethil"
+            let personaje = Personaje 0 100 nombrePersonaje
+            (inicialesDePersonaje personaje) `shouldBe` ["A","M"]
+
+    describe "inicialesDePersonajeValidado" $ do
+        it "Deberia obtener las iniciales de un personaje validado" $ do
+            let personajeValidado = construirPersonajeValidado (validarNombre "Arthas Menethil") 1000
+            inicialesDePersonajeValidado personajeValidado == Exito (["A","M"])
+
+        it "Deberia fallar al ser el nombre del personaje muy corto" $ do
+            let personajeValidado = construirPersonajeValidado (validarNombre "inv") 1000
+            inicialesDePersonajeValidado personajeValidado == Error "El nombre es muy corto"
+
+    describe "dineroPersonajeValidado" $ do
+        it "Deberia obtener el saldo de un personaje validado" $ do
+            let personajeValidado = construirPersonajeValidado (validarNombre "Arthas Menethil") 1000
+            dineroPersonajeValidado personajeValidado == Exito (1000)
+
+        it "Deberia fallar al ser el nombre del personaje muy corto" $ do
+            let personajeValidado = construirPersonajeValidado (validarNombre "inv") 1000
+            dineroPersonajeValidado personajeValidado == Error "El nombre es muy corto"
